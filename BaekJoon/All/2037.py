54336f644e6b9cbcdf2d952a5664b50bc787e1b4
Minkeyyyy <invincible92@naver.com>
2021-09-09 18:09:45 +0900

    Update 2037 2153

--- BaekJoon/All/2037.py
+++ BaekJoon/All/2037.py
@@ -5,18 +5,19 @@
 lss = [' ', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 p, w = map(int, input().split())
 st = input()
-flag = -1
+flag = -1  # 전에 누른 숫자 위치
 cho = 0
 
 for i in st:
     for ls in lss:
         if i in ls:
-            n_flag = lss.index(ls) + 1
-            if n_flag == flag:
+            n_flag = lss.index(ls) + 1  # 어느 숫자를 눌러야 하는지 결정
+            if n_flag == 1 and flag == 1:  # 공백이 연속으로 나오면
+                pass
+            elif n_flag == flag:  # 이전 숫자와 같은 숫자면
                 cho += w
-            flag = n_flag
-            cho += p*(ls.index(i)+1)
-            print(i, cho)
-            break
+            flag = n_flag  # flag를 최신화
+            cho += p*(ls.index(i)+1)  # 문자의 위치에 따라서 누르는 횟수를 결정
+            break  # 나온 것이면 거기서 끝내면 됨
 
 print(cho)
--- BaekJoon/All/2153.py
+++ BaekJoon/All/2153.py
@@ -1,14 +1,13 @@
 # 2021.09.06
 # 2153
 # 소수 단어
-# A = 65 , a = 97
 
 st = input()
 result = 0
 for i in st:
     asc = ord(i)
     if 97 <= asc <= (97+25):
-        result += (asc - ord('a'))
+        result += (asc - ord('a')+1)
     else:
         result += (asc-38)
 
@@ -16,8 +15,8 @@ flag = True
 for i in range(2, result):
     if result % i == 0:
         flag = False
-        print(result, "It is not a prime word.")
+        print("It is not a prime word.")
         break
 
 if flag:
-    print(result, "It is a prime word.")
+    print("It is a prime word.")
