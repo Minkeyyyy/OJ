bcb843f255c55e286dd5b86ad8f042a4605712fc
Minkeyyyy <invincible92@naver.com>
2021-11-22 10:36:54 +0900

    Solved 1075

---
+++ BaekJoon/All/1075.py
@@ -0,0 +1,13 @@
+# 2021.11.22
+# 1075
+# 나누기
+
+n = input()
+f = int(input())
+mini = int(n[:-2]+'00')
+print(mini)
+
+for i in range(mini, mini+100):
+    if i % f == 0:
+        print(str(i)[-2:])
+        break
