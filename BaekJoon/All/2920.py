f2399baa82d67f273a7c6a54e72f4ca8af6ae72c
minggoo <invincible92@naver.com>
2022-02-11 18:36:36 +0900

    Another 2920

--- BaekJoon/All/2920.py
+++ BaekJoon/All/2920.py
@@ -1,14 +1,37 @@
-# 2021.10.12
+# # 2021.10.12
+# # 2920
+# # 음계
+
+# ls = list(map(int, input().split()))
+# ls2 = sorted(ls)
+# ls3 = sorted(ls, reverse=True)
+
+# if ls == ls2:
+#     print("ascending")
+# elif ls == ls3:
+#     print("descending")
+# else:
+#     print("mixed")
+
+# 2022.02.11
 # 2920
 # 음계
+ 
+ls = list(map(int,input().split()))
+ascending = True
+descending = True
 
-ls = list(map(int, input().split()))
-ls2 = sorted(ls)
-ls3 = sorted(ls, reverse=True)
-
-if ls == ls2:
+for i in range(1,8):
+    if ls[i] < ls[i-1]:
+        ascending = False
+    elif ls[i] > ls[i-1]:
+        descending = False
+    else:
+        continue
+    
+if ascending:
     print("ascending")
-elif ls == ls3:
+elif descending:
     print("descending")
 else:
     print("mixed")
